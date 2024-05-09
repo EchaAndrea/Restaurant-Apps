171f23aa9b455843ad7e2c27176d9b92044517f4
Echa Andrea Gustiar <142242034+EchaAndrea@users.noreply.github.com>
2024-05-09 13:08:11 +0700

    Add files via upload

---
+++ steps_file.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line func-names
+module.exports = function () {
+  return actor({
+
+    // Define custom steps here, use 'this' to access default methods of I.
+    // It is recommended to place a general 'login' function here.
+
+  });
+};
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+const path = require('path');
+// eslint-disable-next-line no-unused-vars
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line no-unused-vars
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// eslint-disable-next-line no-unused-vars
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+// eslint-disable-next-line no-unused-vars
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+
+module.exports = {
+  // Kode konfigurasi lainnya disembunyikan...
+};
